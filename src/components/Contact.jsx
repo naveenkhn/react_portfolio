3f4c5b1803857fe24f6143c1198f4523749d9194
kumarn <naveenkmrhn@gmail.com>
2025-05-09 07:21:22 +0530

    Updated resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
         </div>
 
         <p className="contact-description">
-          I’m currently looking for new opportunities. Whether you have a question
-          or just want to say hi, I’ll try my best to get back to you!
+          I’m currently looking for new opportunities. Whether you have a opening
+          or want to add me to your network, I’ll try my best to get back to you!
         </p>
 
         <a href="mailto:naveenkmrhn@gmail.com" className="contact-email-button">
